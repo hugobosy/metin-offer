258d1854111fa923e1926bc0b928ee329c1b19d3
Hubert <h.kliszcz@nebucode.com>
2023-06-21 10:27:52 +0200

    add max width layout and center block

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,14 @@
 import {FC, PropsWithChildren} from "react";
 import styles from './Layout.module.scss';
+import {Header} from "@/components/layout/header/Header";
+import {Footer} from "@/components/layout/footer/Footer";
 
 export const Layout: FC<PropsWithChildren> = ({children}) => {
     return (
         <div className={styles.layout}>
-            <header>Header</header>
+            <Header />
             <main>{children}</main>
-            <footer>Footer</footer>
+            <Footer />
         </div>
     )
 }
